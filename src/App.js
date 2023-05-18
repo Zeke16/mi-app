a6ba695080c5cf264ac89d6b333d8fa04717314b
Zeke16 <kr2000.16@gmail.com>
2023-05-18 14:09:47 -0600

    primer app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Testimonio } from "./components/Testimonio";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenedor-principal">
+        <h1>Testimonios de nuestros trabajadores</h1>
+        <Testimonio
+          nombre="Shawn Wang "
+          pais="Singapore"
+          imagen="1"
+          cargo="Software Engineer"
+          empresa="Amazon"
+          testimonio="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
+        />
+        <Testimonio
+          nombre="Zeke"
+          pais="Singapore"
+          imagen="1"
+          cargo="Software Engineer"
+          empresa="Amazon"
+          testimonio="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
+        />
+      </div>
     </div>
   );
 }
